cf892f7262cd69de21eb40048b9f23c0c7678595
rh2740 <rh2740@nyu.edu>
2018-10-15 17:39:03 -0400

    Initial commit

---
+++ Grab.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+imlist = pd.read_excel('S043_CPTAC_UCEC_Discovery_Cohort_Study_Specimens_r1_Sept2018.xlsx', header=4)
+imlist = imlist[imlist['Group'] == 'Tumor ']
+cllist = pd.read_csv('UCEC_V1.1/ucec_v1.1_clinical_plus.txt', sep='\t', header = 0)
+cllist = cllist[cllist['Proteomics_Tumor_normal'] == 'Tumor']
+
+joined = pd.merge(imlist, cllist, how='inner', on=['Participant_ID'])
+
+joined.to_csv('joined_PID.csv', index = False)
