34802ea02c4b1c1505f7acd99641e1ff5971454f
rh2740 <rh2740@nyu.edu>
2019-01-16 16:39:37 -0500

    1.16.1

---
+++ Scripts/TCGA.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+
+image_meta = pd.read_csv('../TCGA_Image_meta.tsv', sep='\t', header=0)
+TCGA_list = pd.read_excel('../datafile.S1.1.KeyClinicalData.xls', header=0)
+
+namelist = []
+for idx, row in image_meta.iterrows():
+    namelist.append(row['File Name'].split('-01Z')[0])
+
+image_meta['bcr_patient_barcode'] = namelist
+
+TCGA_list = TCGA_list.join(image_meta.set_index('bcr_patient_barcode'), on='bcr_patient_barcode')
+
+TCGA_list = TCGA_list.dropna()
+
+labellist = []
+for idx, row in TCGA_list.iterrows():
+    if row['IntegrativeCluster'] != "Notassigned":
+        labellist.append(row['IntegrativeCluster'])
+    else:
+        labellist.append(row['histology'])
+
+TCGA_list['label'] = labellist
+
+TCGA_list = TCGA_list[TCGA_list.label != 'Mixed']
+TCGA_list = TCGA_list[TCGA_list.label != 'Serous']
+lbdict = {'CN low': 'Endometroid', 'CN high': 'Serous-like'}
+TCGA_list['label'] = TCGA_list['label'].replace(lbdict)
+
+TCGA_list.to_csv('../new_TCGA_list.csv', header=True, index=False)
