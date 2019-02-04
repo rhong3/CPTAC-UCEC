15f91bb308cc2e10ac3fc8705b9c5a35774cf835
rh2740 <rh2740@nyu.edu>
2019-02-04 14:08:32 -0500

    2.4.1

--- Scripts/data_input.py
+++ Scripts/data_input.py
@@ -9,8 +9,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
 import math
+import tensorflow as tf
 
 
 class DataSet(object):
