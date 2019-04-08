c7b74d0e9f18f9f1e725c0bcff6d0561ef4225c6
rh2740 <rh2740@nyu.edu>
2019-04-08 19:16:32 -0400

    4.8.7

--- Scripts/RGB_profiler.py
+++ Scripts/RGB_profiler.py
@@ -50,27 +50,40 @@ def loader(totlist_dir):
             mask = mask*maskb
             masksum = np.sum(mask)
             BB = np.sum(the_imagea[:, :, 0]*mask)/masksum
+            B = np.flatnonzero(the_imagea[:, :, 0]*mask)
             GG = np.sum(the_imagea[:, :, 1]*mask)/masksum
+            G = np.flatnonzero(the_imagea[:, :, 1] * mask)
             RR = np.sum(the_imagea[:, :, 2]*mask)/masksum
-            RGB.append([imlist[i], RR, GG, BB])
+            R = np.flatnonzero(the_imagea[:, :, 2] * mask)
+            RGB.append([imlist[i], RR, np.percentile(R, 25), np.percentile(R, 75), np.std(R),
+                        GG, np.percentile(G, 25), np.percentile(G, 75), np.std(G),
+                        BB, np.percentile(B, 25), np.percentile(B, 75), np.std(B)])
         except AttributeError:
             print('Error image:'+imlist[i])
             pass
 
-    RGBdf = pd.DataFrame(RGB, columns=['Img', 'Red', 'Green', 'Blue'])
+    RGBdf = pd.DataFrame(RGB, columns=['Img', 'Redmean', 'Red25', 'Red75', 'Redstd',
+                                       'Greenmean', 'Green25', 'Green75', 'Greenstd',
+                                       'Bluemean', 'Blue25', 'Blue75', 'Bluestd'])
 
     RGBdf.to_csv('../Results/RGB.csv', index=False, header=True)
 
-    Rmean = RGBdf['Red'].mean()
-    Gmean = RGBdf['Green'].mean()
-    Bmean = RGBdf['Blue'].mean()
-    Rstd = RGBdf['Red'].std()
-    Gstd = RGBdf['Green'].std()
-    Bstd = RGBdf['Blue'].std()
+    Rmean = RGBdf['Redmean'].mean()
+    Gmean = RGBdf['Greenmean'].mean()
+    Bmean = RGBdf['Bluemean'].mean()
+    Rstd = RGBdf['Redstd'].mean()
+    Gstd = RGBdf['Greenstd'].mean()
+    Bstd = RGBdf['Bluestd'].mean()
+    Raqt = RGBdf['Red25'].mean()
+    Gaqt = RGBdf['Green25'].mean()
+    Baqt = RGBdf['Blue25'].mean()
+    Rbqt = RGBdf['Red75'].mean()
+    Gbqt = RGBdf['Green75'].mean()
+    Bbqt = RGBdf['Blue75'].mean()
 
-    print("Red mean={}; std={}".format(str(Rmean), str(Rstd)))
-    print("Green mean={}; std={}".format(str(Gmean), str(Gstd)))
-    print("Blue mean={}; std={}".format(str(Bmean), str(Bstd)))
+    print("Red Mean of: mean={}; std={}; 25pct={}; 75pct={}".format(str(Rmean), str(Rstd), str(Raqt), str(Rbqt)))
+    print("Green Mean of: mean={}; std={}; 25pct={}; 75pct={}".format(str(Gmean), str(Gstd), str(Gaqt), str(Gbqt)))
+    print("Blue Mean of: mean={}; std={}; 25pct={}; 75pct={}".format(str(Bmean), str(Bstd), str(Baqt), str(Bbqt)))
 
 
 if __name__ == "__main__":
