63be382f758428b76b2577ca4973af7122f0a907
rh2740 <rh2740@nyu.edu>
2019-04-14 13:53:56 -0400

    4.14.2

--- Scripts/data_input2.py
+++ Scripts/data_input2.py
@@ -46,10 +46,10 @@ class DataSet(object):
         images = tf.image.rot90(images, k=angles)
         images = tf.image.random_flip_left_right(images)
         images = tf.image.random_flip_up_down(images)
-        images = tf.image.random_hue(images, 0.1)
-        images = tf.image.random_brightness(images, 0.1)
-        images = tf.image.random_contrast(images, 0.8, 1.2)
-        images = tf.image.random_saturation(images, 0.8, 1.2)
+        images = tf.image.random_hue(images, 0.02)
+        images = tf.image.random_brightness(images, 0.02)
+        images = tf.image.random_contrast(images, 0.9, 1.1)
+        images = tf.image.random_saturation(images, 0.9, 1.1)
 
         labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=self._classes)
 
