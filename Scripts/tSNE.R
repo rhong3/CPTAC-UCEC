f30f4abf26b2b184407679676c940f427c4f9593
rh2740 <rh2740@nyu.edu>
2019-08-27 09:45:20 -0400

    8.27.1

--- Scripts/tSNE.R
+++ Scripts/tSNE.R
@@ -50,27 +50,27 @@ p1=ggplot(data=dat,aes(x=tsne1,y=tsne2,col=POS_score))+
   scale_color_gradient2(high='darkorange',mid='white',low='steelblue',midpoint=0.5)+
   geom_point(alpha=0.2)+
   #theme(legend.position='bottom')+
-  xlim(-50,50)+
-  ylim(-50,50)
+  xlim(-60,60)+
+  ylim(-60,60)
 
 p2=ggplot(data=dat,aes(x=tsne1,y=tsne2))+
   geom_point(aes(col=True_label),alpha=0.2)+
   scale_color_manual(values = c('gray70','red'))+
   #theme(legend.position='bottom')+
-  xlim(-50,50)+
-  ylim(-50,50)
+  xlim(-60,60)+
+  ylim(-60,60)
 
 p3=ggplot(data=dat,aes(x=tsne1,y=tsne2))+
   geom_point(aes(col=slide),alpha=0.2)+
   theme(legend.position='none')+
-  xlim(-50,50)+
-  ylim(-50,50)
+  xlim(-60,60)+
+  ylim(-60,60)
 
 p4=ggplot(data=subset(dat,True_label==1),aes(x=tsne1,y=tsne2))+
   geom_point(aes(col=slide),alpha=0.2)+
   theme(legend.position='none')+
-  xlim(-50,50)+
-  ylim(-50,50)
+  xlim(-60,60)+
+  ylim(-60,60)
 
 pdf(file=out_fig,
     width=14,height=7)
