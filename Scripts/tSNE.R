aeee18c57ae259646d49ecedba7c2850fc69c011
rh2740 <rh2740@nyu.edu>
2019-09-11 11:56:12 -0400

    9.11.1

--- Scripts/tSNE.R
+++ Scripts/tSNE.R
@@ -9,9 +9,9 @@
 # bins=args[5]
 # POS_score=args[6]
 
-input_file='/Users/rh2740/documents/CPTAC-UCEC/Results/NL3/I3CTCF/out/For_tSNE.csv'
-output_file='/Users/rh2740/documents/CPTAC-UCEC/Results/NL3/I3CTCF/out/tSNE_P_N.csv'
-out_fig='/Users/rh2740/documents/CPTAC-UCEC/Results/NL3/I3CTCF/out/P_N.pdf'
+input_file='/Users/rh2740/documents/CPTAC-UCEC/Results/NL3/I2FBXW7/out/For_tSNE.csv'
+output_file='/Users/rh2740/documents/CPTAC-UCEC/Results/NL3/I2FBXW7/out/tSNE_P_N.csv'
+out_fig='/Users/rh2740/documents/CPTAC-UCEC/Results/NL3/I2FBXW7/out/P_N.pdf'
 start=9
 bins=50
 POS_score='POS_score'
