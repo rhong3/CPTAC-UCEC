31955301725a1a37e4c77441b9cd2ece5504e578
rh2740 <rh2740@nyu.edu>
2019-10-14 10:08:22 -0400

    10.14.1 bug fix

--- Scripts/Accessory.py
+++ Scripts/Accessory.py
@@ -547,5 +547,6 @@ def tSNE_prep(flatnet, ori_test, y, pred, path, pmd):
     out.reset_index(drop=True, inplace=True)
     act.reset_index(drop=True, inplace=True)
     out = pd.concat([ori_test, out, act], axis=1)
-    out = out.sample(30000, replace=False)
+    if out.shape[0] > 30000:
+        out = out.sample(30000, replace=False)
     out.to_csv("../Results/{}/out/For_tSNE.csv".format(path), index=False)
