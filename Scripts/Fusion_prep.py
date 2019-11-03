b3c62b0d1db6e9412b04489e9574a91fa850a08b
rh2740 <rh2740@nyu.edu>
2019-11-03 15:33:26 -0500

    11.3.1

--- Scripts/Fusion_prep.py
+++ Scripts/Fusion_prep.py
@@ -6,20 +6,24 @@ Created on 11/01/2019
 @author: RH
 """
 import pandas as pd
+import numpy as np
 
 labels = pd.read_csv('../dummy_His_MUT_joined.csv', header=0)
 CPTAC = pd.read_csv('../CPTAC_clinical.csv', header=0)
 CPTAC = CPTAC[CPTAC['Proteomics_Tumor_Normal'] == 'Tumor']
 TCGA = pd.read_excel('../datafile.S1.1.KeyClinicalData.xls', header=0)
 
-TCGA = TCGA[['bcr_patient_barcode', 'age', 'BMI', 'race']]
+TCGA = TCGA[['bcr_patient_barcode', 'age', 'BMI']]
 TCGA = TCGA.rename(columns={'bcr_patient_barcode': 'name'})
-CPTAC = CPTAC[['Participant_ID', 'Age', 'BMI', 'Race']]
-CPTAC = CPTAC.rename(columns={'Participant_ID': 'name', 'Age': 'age', 'Race': 'race'})
+CPTAC = CPTAC[['Participant_ID', 'Age', 'BMI']]
+CPTAC = CPTAC.rename(columns={'Participant_ID': 'name', 'Age': 'age'})
 
 clinical = pd.concat([CPTAC, TCGA], ignore_index=True)
 clinical = clinical.reset_index()
-clinical = clinical[['name', 'age', 'BMI', 'race']]
+clinical = clinical[['name', 'age', 'BMI']]
 new_labels = labels.merge(clinical, left_on='name', right_on='name')
+
+new_labels = new_labels.replace({"Unknown": np.nan})
+
 new_labels.to_csv('../Fusion_dummy_His_MUT_joined.csv', index=False)
 
