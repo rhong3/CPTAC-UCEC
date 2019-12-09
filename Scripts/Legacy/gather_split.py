06c0003a2050423164ef94146fd0bc699ef09710
rh2740 <rh2740@nyu.edu>
2019-12-09 10:15:27 -0500

    12.09.2

--- Scripts/Legacy/gather_split.py
+++ Scripts/Legacy/gather_split.py
@@ -6,6 +6,7 @@ Created on 11/15/2019
 @author: RH
 """
 import pandas as pd
+import numpy as np
 
 flist = ['his', 'MSIst', 'ST', 'ARID1A', 'ATM', 'BRCA2', 'CTCF', 'CTNNB1', 'FAT1', 'FBXW7', 'FGFR2', 'JAK1', 'KRAS',
          'MTOR', 'PIK3CA', 'PIK3R1', 'PPP2R1A', 'PTEN', 'RPL22', 'TP53', 'ZFHX3']
@@ -29,3 +30,44 @@ for i in flist:
     pdpd = pd.concat([trpd, vapd, tepd], ignore_index=True)
     pdpd.columns = ['slide', 'set']
     pdpd.to_csv('../split/{}.csv'.format(i), index=False)
+
+# # For TP53-244 split
+# tr = pd.read_csv('../Results/NL5/X1TP53-244/data/tr_sample.csv', header=0)
+# te = pd.read_csv('../Results/NL5/X1TP53-244/data/te_sample.csv', header=0)
+# va = pd.read_csv('../Results/NL5/X1TP53-244/data/va_sample.csv', header=0)
+# trunq = list(tr.slide.unique())
+# teunq = list(te.slide.unique())
+# vaunq = list(va.slide.unique())
+# tepd = pd.DataFrame(columns=['slide', 'set'])
+# tepd['slide'] = teunq
+# tepd['set'] = 'test'
+# trpd = pd.DataFrame(columns=['slide', 'set'])
+# trpd['slide'] = trunq
+# trpd['set'] = 'train'
+# vapd = pd.DataFrame(columns=['slide', 'set'])
+# vapd['slide'] = vaunq
+# vapd['set'] = 'validation'
+# pdpd = pd.concat([trpd, vapd, tepd], ignore_index=True)
+# pdpd.columns = ['slide', 'set']
+#
+# ref = pd.read_csv('../dummy_His_MUT_joined.csv', header=0)
+# ref = ref[~ref['TP53'].isna()]
+#
+# pdpd = pdpd[pdpd['slide'].isin(ref['name'].tolist())]
+#
+# ref = ref[~ref['name'].isin(pdpd['slide'].tolist())]['name'].tolist()
+#
+# pdlst = []
+# for m in ref:
+#     ppp = np.random.random()
+#     print(ppp)
+#     if ppp < 0.1:
+#         pdlst.append([m, 'test'])
+#     elif ppp > 0.9:
+#         pdlst.append([m, 'validation'])
+#     else:
+#         pdlst.append([m, 'train'])
+#
+# pdpdd = pd.DataFrame(pdlst, columns=['slide', 'set'])
+# pdpd = pdpd.append(pdpdd)
+# pdpd.to_csv('../split/TP53.csv', index=False)
