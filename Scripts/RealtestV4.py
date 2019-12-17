8d0aaab210cc4c45f45acfd0d106b99f0838183a
rh2740 <rh2740@nyu.edu>
2019-12-17 13:39:26 -0500

    12.17.3

--- Scripts/RealtestV4.py
+++ Scripts/RealtestV4.py
@@ -28,111 +28,9 @@ imgfile = sys.argv[2]  # input scn/svs name (eg. TCGA/XXX.scn)
 bs = sys.argv[3]  # batch size
 bs = int(bs)
 md = sys.argv[4]  # loaded model's architecture
-modeltoload = sys.argv[5]  # full path to the trained model to be loaded
-pdmd = sys.argv[6]  # feature to predict
-
-if 'I' in md:
-    import cnn4 as cnn
-    import data_input2 as data_input
-    level = 1
-    loader = loaderI
-    cut = 2
-else:
-    import cnn5 as cnn
-    import data_input3 as data_input
-    level = None
-    loader = loaderX
-    cut = 4
-
-sup = False
-
-if pdmd == 'subtype':
-    classes = 4
-else:
-    classes = 2
-
-print('Input config:')
-print(dirr, imgfile, bs, md, pdmd, sup)
-
-
-# input image dimension
-INPUT_DIM = [bs, 299, 299, 3]
-# hyper parameters
-HYPERPARAMS = {
-    "batch_size": bs,
-    "dropout": 0.3,
-    "learning_rate": 1E-4,
-    "classes": classes,
-    "sup": sup
-}
-
-# paths to directories
-LOG_DIR = "../Results/{}".format(dirr)
-METAGRAPH_DIR = "../Results/{}".format(dirr)
-data_dir = "../Results/{}".format(dirr)
-out_dir = "../Results/{}/out".format(dirr)
-
-
-def tile_ids_in(root_dir, level=1):
-    ids = []
-    try:
-        for id in os.listdir(root_dir):
-            if '.png' in id:
-                ids.append([level, root_dir+'/'+id])
-            else:
-                print('Skipping ID:', id)
-    except FileNotFoundError:
-        print('Ignore:', root_dir)
-    test_tiles = pd.DataFrame(ids, columns=['level', 'L0path'])
-    test_tiles.insert(loc=0, column='Num', value=test_tiles.index)
-    return test_tiles
-
-
-# pair tiles of 10x, 5x, 2.5x of the same area
-def paired_tile_ids_in(root_dir):
-    dira = os.path.isdir(root_dir + 'level1')
-    dirb = os.path.isdir(root_dir + 'level2')
-    dirc = os.path.isdir(root_dir + 'level3')
-    if dira and dirb and dirc:
-        if "TCGA" in root_dir:
-            fac = 2000
-        else:
-            fac = 1000
-        ids = []
-        for level in range(1, 4):
-            dirr = root_dir + 'level{}'.format(str(level))
-            for id in os.listdir(dirr):
-                if '.png' in id:
-                    x = int(float(id.split('x-', 1)[1].split('-', 1)[0]) / fac)
-                    y = int(float(re.split('_', id.split('y-', 1)[1])[0]) / fac)
-                    try:
-                        dup = re.split('.p', re.split('_', id.split('y-', 1)[1])[1])[0]
-                    except IndexError:
-                        dup = np.nan
-                    ids.append([level, dirr + '/' + id, x, y, dup])
-                else:
-                    print('Skipping ID:', id)
-        ids = pd.DataFrame(ids, columns=['level', 'path', 'x', 'y', 'dup'])
-        idsa = ids.loc[ids['level'] == 1]
-        idsa = idsa.drop(columns=['level'])
-        idsa = idsa.rename(index=str, columns={"path": "L0path"})
-        idsb = ids.loc[ids['level'] == 2]
-        idsb = idsb.drop(columns=['level'])
-        idsb = idsb.rename(index=str, columns={"path": "L1path"})
-        idsc = ids.loc[ids['level'] == 3]
-        idsc = idsc.drop(columns=['level'])
-        idsc = idsc.rename(index=str, columns={"path": "L2path"})
-        idsa = pd.merge(idsa, idsb, on=['x', 'y', 'dup'], how='left', validate="many_to_many")
-        idsa['x'] = idsa['x'] - (idsa['x'] % 2)
-        idsa['y'] = idsa['y'] - (idsa['y'] % 2)
-        idsa = pd.merge(idsa, idsc, on=['x', 'y', 'dup'], how='left', validate="many_to_many")
-        idsa = idsa.drop(columns=['x', 'y', 'dup'])
-        idsa = idsa.dropna()
-        idsa = sku.shuffle(idsa)
-    else:
-        idsa = pd.DataFrame(columns=['L0path', 'L1path', 'L2path'])
-
-    return idsa
+modeltoload = sys.argv[5]  # trained model to be loaded
+meta = sys.argv[6]  # full path to the trained model to be loaded
+pdmd = sys.argv[7]  # feature to predict
 
 
 # read images
@@ -214,6 +112,109 @@ def loaderI(totlist_dir):
     writer.close()
 
 
+if 'I' in md:
+    import cnn4 as cnn
+    import data_input2 as data_input
+    level = 1
+    loader = loaderI
+    cut = 2
+else:
+    import cnn5 as cnn
+    import data_input3 as data_input
+    level = None
+    loader = loaderX
+    cut = 4
+
+sup = False
+
+if pdmd == 'subtype':
+    classes = 4
+else:
+    classes = 2
+
+print('Input config:')
+print(dirr, imgfile, bs, md, pdmd, sup, modeltoload, meta)
+
+
+# input image dimension
+INPUT_DIM = [bs, 299, 299, 3]
+# hyper parameters
+HYPERPARAMS = {
+    "batch_size": bs,
+    "dropout": 0.3,
+    "learning_rate": 1E-4,
+    "classes": classes,
+    "sup": sup
+}
+
+# paths to directories
+LOG_DIR = "../Results/{}".format(dirr)
+METAGRAPH_DIR = "../Results/{}".format(meta)
+data_dir = "../Results/{}".format(dirr)
+out_dir = "../Results/{}/out".format(dirr)
+
+
+def tile_ids_in(root_dir, level=1):
+    ids = []
+    try:
+        for id in os.listdir(root_dir):
+            if '.png' in id:
+                ids.append([level, root_dir+'/'+id])
+            else:
+                print('Skipping ID:', id)
+    except FileNotFoundError:
+        print('Ignore:', root_dir)
+    test_tiles = pd.DataFrame(ids, columns=['level', 'L0path'])
+    test_tiles.insert(loc=0, column='Num', value=test_tiles.index)
+    return test_tiles
+
+
+# pair tiles of 10x, 5x, 2.5x of the same area
+def paired_tile_ids_in(root_dir):
+    dira = os.path.isdir(root_dir + 'level1')
+    dirb = os.path.isdir(root_dir + 'level2')
+    dirc = os.path.isdir(root_dir + 'level3')
+    if dira and dirb and dirc:
+        if "TCGA" in root_dir:
+            fac = 2000
+        else:
+            fac = 1000
+        ids = []
+        for level in range(1, 4):
+            dirrr = root_dir + 'level{}'.format(str(level))
+            for id in os.listdir(dirrr):
+                if '.png' in id:
+                    x = int(float(id.split('x-', 1)[1].split('-', 1)[0]) / fac)
+                    y = int(float(re.split('_', id.split('y-', 1)[1])[0]) / fac)
+                    try:
+                        dup = re.split('.p', re.split('_', id.split('y-', 1)[1])[1])[0]
+                    except IndexError:
+                        dup = np.nan
+                    ids.append([level, dirrr + '/' + id, x, y, dup])
+                else:
+                    print('Skipping ID:', id)
+        ids = pd.DataFrame(ids, columns=['level', 'path', 'x', 'y', 'dup'])
+        idsa = ids.loc[ids['level'] == 1]
+        idsa = idsa.drop(columns=['level'])
+        idsa = idsa.rename(index=str, columns={"path": "L0path"})
+        idsb = ids.loc[ids['level'] == 2]
+        idsb = idsb.drop(columns=['level'])
+        idsb = idsb.rename(index=str, columns={"path": "L1path"})
+        idsc = ids.loc[ids['level'] == 3]
+        idsc = idsc.drop(columns=['level'])
+        idsc = idsc.rename(index=str, columns={"path": "L2path"})
+        idsa = pd.merge(idsa, idsb, on=['x', 'y', 'dup'], how='left', validate="many_to_many")
+        idsa['x'] = idsa['x'] - (idsa['x'] % 2)
+        idsa['y'] = idsa['y'] - (idsa['y'] % 2)
+        idsa = pd.merge(idsa, idsc, on=['x', 'y', 'dup'], how='left', validate="many_to_many")
+        idsa = idsa.drop(columns=['x', 'y', 'dup'])
+        idsa = idsa.dropna()
+    else:
+        idsa = pd.DataFrame(columns=['L0path', 'L1path', 'L2path'])
+
+    return idsa
+
+
 # load tfrecords and prepare datasets
 def tfreloader(bs, cls, ct):
     filename = data_dir + '/test.tfrecords'
@@ -224,7 +225,7 @@ def tfreloader(bs, cls, ct):
 
 # main function for real test image prediction
 def test(bs, cls, to_reload):
-    m = cnn.INCEPTION(INPUT_DIM, HYPERPARAMS, meta_graph=to_reload, log_dir=LOG_DIR, meta_dir=LOG_DIR, model=md)
+    m = cnn.INCEPTION(INPUT_DIM, HYPERPARAMS, meta_graph=to_reload, log_dir=LOG_DIR, meta_dir=METAGRAPH_DIR, model=md)
 
     print("Loaded! Ready for test!")
     HE = tfreloader(bs, cls, None)
@@ -288,7 +289,7 @@ if __name__ == "__main__":
     if not os.path.isfile(data_dir + '/test.tfrecords'):
         loader(data_dir)
     test(bs, classes, to_reload=modeltoload)
-    slist = pd.read_csv(data_dir + '/tr_sample.csv', header=0)
+    slist = pd.read_csv(data_dir + '/te_sample.csv', header=0)
     # load dictionary of predictions on tiles
     teresult = pd.read_csv(out_dir+'/Test.csv', header=0)
     # join 2 dictionaries
