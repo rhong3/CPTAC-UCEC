21a00cff880724150109c987a1f716fe1d545567
rh2740 <rh2740@nyu.edu>
2020-02-03 16:25:34 -0500

    2.3.2

--- Scripts/FPFN.py
+++ Scripts/FPFN.py
@@ -2,7 +2,7 @@ import pandas as pd
 from shutil import copy2
 import os
 
-TODO = ['X1his', 'F1FAT1', 'F4MSI', 'X1PTEN', 'X1TP53', 'X2SL', 'X2ZFHX3', 'X3CNVH']
+TODO = ['X1his', 'F1FAT1', 'F4MSIst', 'X1PTEN', 'X1TP53', 'X2SL', 'X2ZFHX3', 'X3CNVH']
 try:
     os.mkdir('../Results/NL5/FPFN')
 except FileExistsError:
@@ -11,7 +11,7 @@ except FileExistsError:
 for i in TODO:
     if i == 'X1his':
         POS_score = 'Serous_score'
-    elif i == 'F4MSI':
+    elif i == 'F4MSIst':
         POS_score = 'MSI-H_score'
     else:
         POS_score = 'POS_score'
@@ -28,8 +28,8 @@ for i in TODO:
     except FileExistsError:
         pass
     ref = pd.read_csv('../Results/NL5/{}/out/Test_tile.csv'.format(i), header=0)
-    FP = ref.loc[ref[POS_score] > 0.95 and ref['label'] == 0]
-    FN = ref.loc[ref[POS_score] < 0.05 and ref['label'] == 1]
+    FP = ref[(ref[POS_score] > 0.95) & (ref['label'] == 0)]
+    FN = ref[(ref[POS_score] < 0.05) & (ref['label'] == 1)]
     for idx, row in FP.iterrows():
         copy2(row['L0path'], '../Results/NL5/FPFN/{}/FP/'.format(i))
     for idx, row in FN.iterrows():
