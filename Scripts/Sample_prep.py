e74a334352b00c633aa516c7bc94788a2ff305e1
HONGRUNYU <michaelhong1995@gmail.com>
2020-05-16 14:07:46 -0400

    5.16.1

--- Scripts/Sample_prep.py
+++ Scripts/Sample_prep.py
@@ -145,7 +145,7 @@ def set_sep_secondary(alll, path, cls, pmd, level=None, batchsize=64):
     elif pmd == 'histology':
         split = pd.read_csv('../split/his.csv', header=0)
     elif pmd == 'Serous-like':
-        split = pd.read_csv('../split/SL.csv', header=0)
+        split = pd.read_csv('../split/CNVH.csv', header=0)
     else:
         split = pd.read_csv('../split/{}.csv'.format(pmd), header=0)
     train = split.loc[split['set'] == 'train']['slide'].tolist()
