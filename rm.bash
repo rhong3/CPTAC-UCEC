4a83b4dba37352362a25622f9ee9a68bd30b027b
rhong3 <michaelhong1995@gmail.com>
2020-10-09 13:56:29 -0400

    10.9.1

---
+++ rm.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while sleep 15:;
+	do
+	rm -rf ~/documents/CPTAC-UCEC/Results/NL9/*/*00000-of-00001 ~/documents/CPTAC-UCEC/Results/NL9/*/out/*img ~/documents/CPTAC-UCEC/Results/NL9/*/validation ~/documents/CPTAC-UCEC/Results/NL9/*/training
+done	
