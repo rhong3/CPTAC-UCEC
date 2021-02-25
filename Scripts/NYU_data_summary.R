c868cdbb15ab026490dd5d466c8e6ea1f4c3c957
rhong3 <michaelhong1995@gmail.com>
2021-02-25 09:21:47 -0500

    2.25.2

--- Scripts/NYU_data_summary.R
+++ Scripts/NYU_data_summary.R
@@ -113,7 +113,10 @@ pdf(file = paste('~/documents/GYN-HE-IHC/align/align_eval.pdf',sep='/'),
 ggplot(align, aes(Align_score)) +
   geom_histogram() + 
   labs(x='Align Score (median=0.91776)') +
-  geom_vline(aes(xintercept = median(Align_score)),col='red',size=0.5)
+  geom_vline(aes(xintercept = median(Align_score)),col='red',size=0.5) + 
+  theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(), 
+                     axis.line = element_line(colour = "black"), legend.position='none')
 dev.off()
 
 
